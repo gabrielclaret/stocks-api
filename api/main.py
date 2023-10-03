228e8826a18a871989f960a78a3c96abd134ac4f
gabriel claret <gclaret@outlook.com>
2023-10-02 22:45:06 -0300

    chore: update api configs

--- api/main.py
+++ api/main.py
@@ -1,9 +1,18 @@
+import logging
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from api.config.logging import RequestIdFilter, RequestIdFormatterHandler
+from api.config.settings import get_settings
+from api.middlewares.logging import RequestIdLoggingMiddleware
 from api.routes import api
 
+LOG_LEVEL = logging.getLevelName("INFO")
+
 app = FastAPI()
+
+settings = get_settings()
 
 origins = ["*"]
 
@@ -15,4 +24,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(api.endpoint_router)
+
+def setup_logging() -> None:
+    """Configure logging across entire app."""
+    loggers = list(logging.root.manager.loggerDict.keys())
+    loggers.append("root")
+    for name in loggers:
+        logger = logging.getLogger(name)
+        logger.handlers = [RequestIdFormatterHandler()]
+        logger.filters = [RequestIdFilter("request_id")]
+        logger.level = LOG_LEVEL
+        logger.propagate = False
+
+
+@app.on_event("startup")
+def app_startup_event() -> None:
+    """Startup event."""
+    setup_logging()
+
+
+app.add_middleware(RequestIdLoggingMiddleware)
+app.include_router(api.endpoint_router, prefix=settings.API_V1_PREFIX)
